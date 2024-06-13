9df56ecb66045d242fcf9678bfd53dec3b7d9a6e
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2024-06-13 15:58:04 -0700

    Added a filter button
    filters based on current level

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 from tkinter import *
 from tkinter import ttk
 #TODO maybe read csv file to get all names
+class Student:
+    students=[]
+    def __init__(self,name: str,level: str,subject="Python"):
+        self.name=name
+        self.level=level
+        self.subject=subject
+        __class__.students.append(self)
+#add some random students
+#TODO make this based on reading some weird file or something
+def makeStudents():
+    Student("Bobina","Rookie")
+    Student("Bobby","Rookie")
+    Student("Guy","Rookie")
+    Student("Jiafei","Advanced")
+    Student("Candice","Intermediate")
+    Student("Joe Mama","Intermediate")
+makeStudents()
 def getNames():
-    return ["Bob","Joe Mama"]
+    names=[]
+    for stu in Student.students:
+        names.append(stu.name)
+    return names
 def getSubject():
     return ["Python","VEX"]
 def getInstructors():
@@ -20,15 +40,35 @@ class Option:
         self.wid=wid
         wid.grid(row=len(__class__.menu),column=1)
         __class__.menu.append(self)
+    @staticmethod
+    def getOption(name: str):
+        for opt in __class__.menu:
+            if opt.label.cget("text")==name:
+                return opt
+
+class OptionButton(Option):
+    def __init__(self, root: Tk, label: str, wid: Widget, button: Button):
+        super().__init__(root,label,wid)
+        self.button=button
+        button.grid(row=len(super().menu)-1,column=2)
     
 root = Tk()
 root.geometry("600x600")
 root.resizable(0, 0)
 root.title("Evaluation menu")
 root.grid_columnconfigure(0, weight=1)
-
-Option(root,"Name", ttk.Combobox(root, values=getNames()))
-Option(root,"Levels", ttk.Combobox(root, values=getLevels()))
+namesBox=ttk.Combobox(root, values=getNames())
+Option(root,"Name", namesBox)
+levels=ttk.Combobox(root, values=getLevels())
+def filterLevels():
+    level=levels.get()
+    names=[]
+    for stu in Student.students:
+        if(stu.level==level):
+            names.append(stu.name)
+    namesBox.configure(values=names)
+    namesBox.set("")
+OptionButton(root,"Levels", levels, Button(root,text="Filter",command=filterLevels))
 Option(root,"Subject",ttk.Combobox(root, values=getSubject()))
 Option(root,"Rating", Scale(root,from_=1,to_=5,orient=HORIZONTAL))
 Option(root,"Title", Entry(root))
@@ -48,7 +88,6 @@ def submitButton():
             data[opt.label.cget("text")]=opt.wid.widget.get()
         elif(isinstance(opt.wid,Text)):
             data[opt.label.cget("text")]=opt.wid.get("1.0",END)
-
     print(data)
 def clearButton():
     for opt in Option.menu:
